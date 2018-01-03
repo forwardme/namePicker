058e2b6067afd625ae01dcf3eea3d777226c75be
GaoYuan <forwardme@126.com>
2018-01-03 21:22:28 +0800

    add decode
    string encode to bytes
    bytes decode to string

--- pickName.py
+++ pickName.py
@@ -1,14 +1,14 @@
-﻿
+﻿#coding = gbk
 def ngrams(input,n):
-  input = input.split(' ')
+  input = input.split('\r\n')
   output = []
   for i in range(len(input)-n+1):
     output.append(input[i:i+n])
   return output
   
-with open("/(001)国风·周南.txt","rb") as file:
-  text = file.read()
+with open("(001)国风·周南.txt","rb") as file:
+  text = file.read().decode('gbk')
 
 ngrams = ngrams(text,2)
-print ngrams
+print ngrams[1][0]
 print "2-grams count is: "+str(len(ngrams))

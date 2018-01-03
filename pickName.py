fa304d2aaa9a470f8d2df941d5e3cb47a2fe99c3
GaoYuan <forwardme@126.com>
2018-01-03 21:25:58 +0800

    Revert "add decode"
    This reverts commit 058e2b6067afd625ae01dcf3eea3d777226c75be.

--- pickName.py
+++ pickName.py
@@ -1,14 +1,14 @@
-﻿#coding = gbk
+﻿
 def ngrams(input,n):
-  input = input.split('\r\n')
+  input = input.split(' ')
   output = []
   for i in range(len(input)-n+1):
     output.append(input[i:i+n])
   return output
   
-with open("(001)国风·周南.txt","rb") as file:
-  text = file.read().decode('gbk')
+with open("/(001)国风·周南.txt","rb") as file:
+  text = file.read()
 
 ngrams = ngrams(text,2)
-print ngrams[1][0]
+print ngrams
 print "2-grams count is: "+str(len(ngrams))

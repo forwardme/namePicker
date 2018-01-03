67909458e4e3046b11fd6fb6d1ee70001b87ea75
GaoYuan <forwardme@126.com>
2018-01-03 11:09:25 +0800

    测试

---
+++ pickName.py
@@ -0,0 +1,14 @@
+﻿
+def ngrams(input,n):
+  input = input.split(' ')
+  output = []
+  for i in range(len(input)-n+1):
+    output.append(input[i:i+n])
+  return output
+  
+with open("/(001)国风·周南.txt","rb") as file:
+  text = file.read()
+
+ngrams = ngrams(text,2)
+print ngrams
+print "2-grams count is: "+str(len(ngrams))

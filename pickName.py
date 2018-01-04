f287e67678711f5b9d5384c5ee4d8a842d714a66
forwardme <forwardme@126.com>
2018-01-04 21:03:59 +0800

    数据清洗
    用正则表达式清洗格式和标点字符

--- pickName.py
+++ pickName.py
@@ -1,14 +1,26 @@
 ﻿#coding = gbk
+import re
+def cleanInput(input):
+  output = re.sub('[\n\r]+'," ",input)
+  output = re.sub(ur'[\uff0c\u3002\uff1a\u25cb]+'," ",output)
+  output = re.sub(' +'," ",output)
+  return output
 def ngrams(input,n):
-  input = input.split('\r\n')
+  input = input.split(' ')
   output = []
   for i in range(len(input)-n+1):
     output.append(input[i:i+n])
   return output
   
-with open("(001)国风·周南.txt","rb") as file:
+with open("001.txt","rb") as file:
   text = file.read().decode('gbk')
 
-ngrams = ngrams(text,2)
-print ngrams[1][0]
+
+ngrams = ngrams(cleanInput(text),2)
+print ngrams
 print "2-grams count is: "+str(len(ngrams))
+with open('outputdata.txt','w') as file:
+  for items in ngrams:
+    for item in items:
+      file.writelines(item.encode('utf-8'))
+    file.writelines('\n')

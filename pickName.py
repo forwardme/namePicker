677c1ff4eafb3c90c60786cff09dac4064de7340
GaoYuan <forwardme@126.com>
2018-01-08 21:07:31 +0800

    完成版

--- pickName.py
+++ pickName.py
@@ -1,6 +1,6 @@
 ﻿#coding = gbk
 import re
-
+import operator
 def cleanInput(input):
   output = re.sub('[\n\r]+'," ",input)
   output = re.sub(ur'[\uff0c\u3002\uff1a\u25cb]+'," ",output)
@@ -8,9 +8,12 @@ def cleanInput(input):
   return output
 def ngrams(input,n):
   input = list(input)
-  output = []
+  output = {}
   for i in range(len(input)-n+1):
-    output.append(tuple(input[i:i+n]))
+    ngramTemp = " ".join(input[i:i+n])
+    if ngramTemp not in output:
+      output[ngramTemp] = 0
+    output[ngramTemp] += 1
   return output
   
 with open("001.txt","rb") as file:
@@ -18,11 +21,12 @@ with open("001.txt","rb") as file:
 
 
 ngrams = ngrams(cleanInput(text),2)
-
-print ngrams
+#print ngrams
+sortedNgrams = sorted(ngrams.items(),key=operator.itemgetter(1),reverse=True)
+#print sortedNgrams
 print "2-grams count is: "+str(len(ngrams))
 with open('outputdata.txt','w') as file:
-  for items in ngrams:
-    for item in items:
-      file.writelines(item.encode('utf-8'))
+  for items in sortedNgrams:
+    name,frequency = items
+    file.writelines(name.encode('utf-8')+"  "+str(frequency))
     file.writelines('\n')
